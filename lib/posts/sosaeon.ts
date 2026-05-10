e0c18aaecbf41e0c38e46289446a9914e74d1a36
deuniverse <ay7092@gmail.com>
2026-05-10 21:17:09 +0900

    Update sosaeon.ts

---
+++ lib/posts/sosaeon.ts
@@ -0,0 +1,10 @@
+export const gongmingu = {
+    slug: 'sosaeon',
+    title: '소사언 탐구 생활',
+    subject: 'SO SA-EON',
+    date: '2025-11-27',
+    status: 'PSYCH_ANALYSIS',
+    dangerLevel: 'MODERATE',
+    content: `
+    `
+};
